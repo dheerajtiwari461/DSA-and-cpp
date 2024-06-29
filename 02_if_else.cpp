f9cd5d9f300c4b17b6272ee5f1522139043a73d1
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-29 11:05:51 +0530

    chore: Add new C++ code files and configuration files

---
+++ 02_if_else.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+ using namespace std;
+// Day 4
+int main() {
+    int a;
+    cin >> a;
+
+    if ((a>10) && (a > 12) || (a == 10) && (a == 12))
+    {
+        cout << "accepted";
+    }else{
+        cout << "rejected";
+    }
+    
+}
