c1c82acc6c40a21af27255d2975fe03d8bcf6e2a
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-06-29 12:38:34 +0530

    refactor: Update Day4_if_else.cpp with new code snippets

--- Day4_if_else.cpp
+++ Day4_if_else.cpp
@@ -2,14 +2,124 @@
  using namespace std;
 // Day 4
 int main() {
-    int a;
-    cin >> a;
+    // int a;
+    // cin >> a;
 
-    if ((a>10) && (a > 12) || (a == 10) && (a == 12))
-    {
-        cout << "accepted";
-    }else{
-        cout << "rejected";
+    // if ((a>10) && (a > 12) || (a == 10) && (a == 12))
+    // {
+    //     cout << "accepted";
+    // }else{
+    //     cout << "rejected";
+    // }
+
+//    1. Two numbers are given, print the bigger number, It is given that
+// both numbers can’t be the same.
+
+    // int a, b;
+    // cin >> a >> b;
+
+    // if (a > b) {
+    //     cout << a;
+    // }
+    // else
+    // {
+    //     cout << b;
+    // }
+
+    // 2. Age of a person is given, print Adult if his/her age is greater
+// than 18, otherwise print Teenager.
+
+// int age;
+// cin >> age;
+// if(age>18){
+//     cout << "Adult";
+// }else{
+//     cout << "Teenager";
+// }
+
+// 3. Take a number in input (ex n) and print the corresponding
+// month to it. Ex: for n=1, print january, n=2, print feburary like
+// this you need to give output. It is given that n will be greater
+// than 0 and less than 13.
+
+// int n;
+// cin >> n;
+
+// if(n==1){
+//     cout << "january";
+// }else if(n==2){
+//     cout << "feburary";
+// }else if(n==3){
+//     cout << "march";
+// }else if(n==4){
+//     cout << "april";
+// }else if(n==5){
+//     cout << "may";
+// }else if(n==6){
+//     cout << "june";
+// }else if(n==7){
+//     cout << "july";
+// }else if(n==8){
+//     cout << "august";
+// }else if(n==9){
+//     cout << "september";
+// }else if(n==10){
+//     cout << "october";
+// }else if(n==11){
+//     cout << "november";
+// }else if(n==12){
+//     cout << "december";
+// }else{
+//     cout << "invalid";
+// }
+  
+//   4. Ticket Price Calculator: Create a program that asks the user for
+// their age and checks if they qualify for a discounted ticket price
+// (e.g., under 12 and over 65 get discounts), If they are eligible
+// print “YES” else “NO”.
+
+// int age;
+// cin >> age;
+
+// if(age<12){
+//     cout << "NO";
+// }else if(age>65){
+//     cout << "NO";
+// }else{
+//     cout << "YES";
+// }
+
+
+// For Loop
+// 1. Print “India will win t”, 20 times.
+
+// for(int i=0;i<20;i++){
+//     cout << "India will win " << endl;
+// }
+
+// 2. Print all Odd numbers from 1 to n, take n as an input from the
+// user.
+
+// int n;
+// cin >> n;
+
+// for(int odd =1; odd < n; odd = odd+2){
+//     cout <<  " odd number: " << odd << endl;
+// }
+
+// 3. Print all numbers from 1 to n, which is divisible by 4. Take n as
+// an input from the user.
+
+    int n;
+    cin >> n;
+
+    for(int i =1; i < n; i++){
+        // cout << i << endl;
+        if(i%4==0)
+        cout << i << endl;
+        // }else{
+        
+        
     }
-    
+
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+the World Cup 2023
