7dbd673b5f982d43e32c76e5aad1b43960b3e455
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-07-13 20:23:30 +0530

    Operators

---
+++ Day_1_To_30/Day10_Operators.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+// 1: Temperature Range: Write a program that checks if a given temperature
+// is suitable for swimming. If the temperature is between 70 and 90
+// (Excluded) degrees Fahrenheit print yes, else NO.
+// int temp;
+// cin >> temp;
+// if(temp > 70 && temp < 90) cout << "Yes";
+// else cout <<"No";
+
+// 2: Even and Positive Number: Write a program that prints “YES” if a given
+// number is both even and positive, otherwise it will print “NO”.
+// int num;
+// cin >> num;
+// if(num >0 && num%2==0) cout <<"YES ";
+// else cout <<"NO";
+
+// 3: Age Check: Implement a program that checks if a person is a teenager.
+// A teenager is someone whose age is between 13 and 19 (inclusive).
+// int age;
+// cin>> age;
+// if(age > 13 && age<19) cout << "Teenager";
+// else cout << "Adult ";
+
+// 4: Take three numbers a,b,c from the user, print yes if a is either greater
+// than b or c. Otherwise print NO.
+
+int a,b,c;
+cin >> a>>b>>c;
+if(a>b && a>b) cout << " Greater ";
+else cout << "NO ";
+
+
+}
