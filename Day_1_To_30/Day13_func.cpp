4a8d60171cc69612339f8c67b90330892c616334
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-08-21 18:04:55 +0530

    Add new file Day_1_To_30/Day13_func.cpp and implement sum and sum2 functions

---
+++ Day_1_To_30/Day13_func.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int sum(int add){
+    return add + 4;
+}
+int sum2(int add){
+    return add+5;
+}
+int main()
+{
+   int a,b;
+   cin >>a>>b;
+   // int c = sum(a);
+   // int d = sum2(b);
+ if(sum(a) >sum2(b)) cout <<"greater";
+ else cout << "Lesser";
+
+}
