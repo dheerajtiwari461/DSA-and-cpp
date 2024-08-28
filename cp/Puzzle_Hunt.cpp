463f0c5a5036fefacc8b8490a89dd94cb61985bf
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-08-28 19:50:47 +0530

    feat: Add code for solving DSA problems

---
+++ cp/Puzzle_Hunt.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int x;
+   cin>>x;
+    (x>=6 && x<=8)?cout<<"Yes"<<endl:cout<<"No"<<endl;
+
+    return 0;
+}
+
---
+++ cp/Second_Max_of_Three_Numbers.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+    int x[3];
+    cin>>x[0]>>x[1]>>x[2];
+    sort(x, x+3);
+    cout<<x[1]<<endl; 
+    
+    
+   } 
+    return 0;
+}
+
