e6e07ad130e031822f32901e0faa3053c6d1852e
Dheeraj Tiwari <dheerajtiwari461@outlook.com>
2024-08-30 18:04:50 +0530

    feat: Add code for solving DSA problems

---
+++ cp/Rating_Improvement.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+   int t;
+   cin>>t;
+   while(t--){
+    int x,y;
+    cin >>x>>y;
+    int c=x;
+    (y>=x && y<= x+200)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+   } 
+    return 0;
+}
+
